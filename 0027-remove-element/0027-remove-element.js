12b9e08644ab325afe1293d99170053615831564
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-03-09 22:06:44 -0800

    Time: 67 ms (46.28%), Space: 41.9 MB (71.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let l = 0;
+    for ( let r = 0; r < nums.length; r++){
+        if (nums[r] !== val){
+            nums[l++] = nums[r];
+        } 
+    }
+    return l;
+};
