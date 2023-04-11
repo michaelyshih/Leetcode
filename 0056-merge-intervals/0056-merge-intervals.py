effc6330cf43f95161197bf10146fa982a53ca7a
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-04-11 09:11:01 -0700

    Time: 154 ms (42.96%), Space: 18 MB (98.79%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,22 @@
+class Solution:
+    def merge(self, intervals):
+        intervals.sort()
+        stack = [intervals[0]]
+
+        for i in range(1,len(intervals)):
+            currIntvl = {
+                'start': intervals[i][0],
+                'end': intervals[i][1]
+            }
+
+            lastIntvl = {
+                'start': stack[-1][0],
+                'end': stack[-1][1]
+            }
+
+            if currIntvl['start'] <= lastIntvl['end'] and currIntvl['end'] > lastIntvl['end']: 
+                stack[-1][1] = currIntvl['end']
+            elif currIntvl['start'] > lastIntvl['end']: 
+                stack.append(intervals[i])
+
+        return stack
