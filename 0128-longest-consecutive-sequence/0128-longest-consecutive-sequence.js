141cfb316362eeb4edf4f429b8a54889559545de
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-04-11 13:21:08 -0700

    Time: 518 ms (30.26%), Space: 57.2 MB (68.28%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    let num_set = new Set(nums);
+    let longest = 0;
+    for (let num of nums){
+        if (!num_set.has(num - 1)){
+            let length = 0
+            while (num_set.has(num + length)){
+                length += 1;
+            }
+            longest = Math.max(longest, length)
+        }
+    }
+    return longest
+};
