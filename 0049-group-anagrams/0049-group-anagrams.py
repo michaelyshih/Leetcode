8165cd87866275c0e4b4109d6b57ab9e8d240600
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-04-11 12:50:19 -0700

    Time: 91 ms (93.15%), Space: 17.8 MB (52.81%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,14 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dictionary = {}
-        for word in strs:
-            print(''.join(sorted(word)))
-            sortedWord = ''.join(sorted(word))
-            # If word is not in dictionary
-            if sortedWord not in dictionary:
-                dictionary[sortedWord] = [word]
-            # If previously it is present that means its anagram
-            # was previously present
-            else:
-                dictionary[sortedWord] += [word]
-        return [dictionary[i] for i in dictionary]
+        groups = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            groups[key].append(s)
+
+        return groups.values()
