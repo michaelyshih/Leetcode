277296805ad31d09c36ed1c5f8e3bfe51fde7059
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-04-20 15:10:10 -0700

    Time: 1618 ms (32.18%), Space: 14.2 MB (56.16%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -6,19 +6,28 @@ class Solution:
         # after loop, amount never equals to combination return -1 
         # 0 count as a number
         
-        def _dfs(rem):
-            if rem == 0: return 0 
-            if rem in memo: return memo[rem]
+#         def _dfs(rem):
+#             if rem == 0: return 0 
+#             if rem in memo: return memo[rem]
             
-            memo[rem] = float("inf")
+#             memo[rem] = float("inf")
             
+#             for coin in coins: 
+#                 if rem - coin >= 0:
+#                     memo[rem] = min(memo[rem], _dfs(rem - coin)+1)
+            
+#             return memo[rem]
+        
+#         memo = defaultdict(int)
+#         result = _dfs(amount)
+        
+#         return result if result != float("inf") else -1  
+
+        dp = [float("inf") for _ in range(amount+1)]
+        dp[0] = 0
+        
+        for value in range(1, amount +1):
             for coin in coins: 
-                if rem - coin >= 0:
-                    memo[rem] = min(memo[rem], _dfs(rem - coin)+1)
-            
-            return memo[rem]
-        
-        memo = defaultdict(int)
-        result = _dfs(amount)
-        
-        return result if result != float("inf") else -1  
+                if value - coin >= 0: 
+                    dp[value] = min(dp[value], dp[value-coin] + 1)
+        return -1 if dp[amount] == float("inf") else dp[amount]
