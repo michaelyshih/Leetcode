59dd3b51090b4d913aedd9d7ee52722d3d4ec179
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-04-20 11:36:57 -0700

    Time: 1990 ms (13.81%), Space: 44.4 MB (5.49%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # amount = combination  return fewest min (fewest, coin count)
+        # want to loop from the back 
+        # after loop, amount never equals to combination return -1 
+        # 0 count as a number
+        
+        def _dfs(rem):
+            if rem == 0: return 0 
+            if rem in memo: return memo[rem]
+            
+            memo[rem] = float("inf")
+            
+            for coin in coins: 
+                if rem - coin >= 0:
+                    memo[rem] = min(memo[rem], _dfs(rem - coin)+1)
+            
+            return memo[rem]
+        
+        memo = defaultdict(int)
+        result = _dfs(amount)
+        
+        return result if result != float("inf") else -1  
