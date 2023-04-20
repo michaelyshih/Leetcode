0f520adc6028cdd391e205bad221fe3d04df54fc
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-04-20 13:32:15 -0700

    Time: 27 ms (90.90%), Space: 13.8 MB (96.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,27 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
+#         memo = {}
         
-        def traverse(m,n):
-            if m == 0 or n == 0:
-                return 0 
-            if m == 1 or n == 1:
-                return 1 
-            key = f'{m},{n}'
+#         def traverse(m,n):
+#             if m == 0 or n == 0:
+#                 return 0 
+#             if m == 1 or n == 1:
+#                 return 1 
+#             key = f'{m},{n}'
             
-            if key in memo: return memo[key]
+#             if key in memo: return memo[key]
             
-            memo[key] = traverse(m-1, n) + traverse(m, n-1)
+#             memo[key] = traverse(m-1, n) + traverse(m, n-1)
             
-            return memo[key] 
+#             return memo[key] 
         
-        return traverse(m,n)
+#         return traverse(m,n)
+
+        # tabulation
+        dp = [[1] * n for _ in range(m)]
+
+        for col in range(1,m):
+            for row in range(1,n):
+                dp[col][row] = dp[col-1][row] + dp[col][row-1]
+
+        return dp[-1][-1]
