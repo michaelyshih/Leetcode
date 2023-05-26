70ec8e42660e0b1c792dd2efa274e3df34824427
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-05-26 01:08:11 -0700

    Time: 239 ms (26.29%), Space: 60.8 MB (17.48%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    results = []
+//     sort reads negatives as string and sort them by integer
+    nums.sort((a,b)=> a - b)
+    console.log(nums)
+    
+    for (let [i,ele] of nums.entries()){
+        if (i > 0 && nums[i-1] == ele){
+            continue 
+        }
+        let [l,r] = [i + 1, nums.length -1]
+        
+        while (l < r){
+            let total = ele + nums[l] + nums[r] 
+            if (total > 0){
+                r -= 1 
+            }else if(total < 0){
+                l += 1
+            }else{
+                results.push( [ele, nums[l], nums[r]])
+                l += 1
+                while (nums[l] == nums[l-1] && l < r){
+                    l += 1
+                }
+            }
+        }
+    }
+    return results 
+};
