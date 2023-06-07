24951ed9772a408e703b4a0c91437abaa3f7d4bb
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-06-06 23:10:53 -0700

    Time: 70 ms (82.94%), Space: 17.3 MB (20.93%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,24 +1,33 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         
-        passed = set([])
-        obtainedKeys = set([0])
+#         passed = set([])
+#         obtainedKeys = set([0])
         
-        def _dfs(rooms, obtainedKeys, passed):
+#         def _dfs(rooms, obtainedKeys, passed):
 
-            for roomNum, room in enumerate(rooms):
-                if not roomNum in passed and roomNum in obtainedKeys:
-                    print("checking room:", roomNum)
-                    passed.add(roomNum)
-                    print("passed:", list(passed))
-                    for key in room:
-                        print("nextrooms:", key)
-                        obtainedKeys.add(key)
-                        _dfs(rooms,obtainedKeys, passed)
-                    if len(passed) == len(rooms): return True
+#             for roomNum, room in enumerate(rooms):
+#                 if not roomNum in passed and roomNum in obtainedKeys:
+#                     print("checking room:", roomNum)
+#                     passed.add(roomNum)
+#                     print("passed:", list(passed))
+#                     for key in room:
+#                         print("nextrooms:", key)
+#                         obtainedKeys.add(key)
+#                         _dfs(rooms,obtainedKeys, passed)
+#                     if len(passed) == len(rooms): return True
                     
-            return False 
+#             return False 
         
-        return _dfs(rooms,obtainedKeys, passed)
+#         return _dfs(rooms,obtainedKeys, passed)
+        visited = set()
+        def _dfs(room):
+            visited.add(room)
+            for i in rooms[room]:
+                if i not in visited:
+                    _dfs(i)
+        _dfs(0)
+        
+        return len(visited) == len(rooms)
         
                 
