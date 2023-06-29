aa89e8d0c313f158b1d7ab3e579aa54767c70446
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-06-28 22:09:22 -0700

    Time: 64 ms (31.96%), Space: 19 MB (56.40%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#         run through list if count is even connect to next next  
+#         if count is odd then create a dummy and attach to the end of even linked list when next is undefined 
+#         when at end of odd, connect all the evens 
+#         when at end of even, put into the end and prev is the last connection
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next or not head.next.next:
+            return head 
+        
+        oddP = current = head 
+        eveP = evenhead = head.next 
+        i = 1
+        
+        while current: 
+            if i > 2 and i % 2 != 0:
+                oddP.next = current 
+                oddP = oddP.next
+                
+            elif i > 2 and i % 2 == 0:
+                eveP.next = current
+                eveP = eveP.next
+            current = current.next 
+            i += 1 
+        eveP.next = None 
+        oddP.next = evenhead
+        
+        
+        return head
+                
+                
+            
