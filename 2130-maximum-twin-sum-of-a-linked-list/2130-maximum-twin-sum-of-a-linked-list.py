22aba9eae0f3aec74ffcdcde5d702b5b3037121a
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-06-29 22:43:41 -0700

    Time: 778 ms (93.71%), Space: 38.3 MB (87.13%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,18 +5,35 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        vals = []
-        pair = defaultdict(int)
+        slow, fast = head, head 
+        prev  = None 
+        while fast and fast.next: 
+            fast = fast.next.next 
+            temp = slow.next 
+            slow.next = prev 
+            prev = slow 
+            slow = temp
+            
+        res = 0
+        while slow:
+            res = max(res, prev.val + slow.val)
+            prev = prev.next 
+            slow = slow.next
+            
+        return res
+            
+#         vals = []
+#         pair = defaultdict(int)
         
-        while head:
-            vals.append(head.val)
-            head = head.next
-        n = len(vals)
+#         while head:
+#             vals.append(head.val)
+#             head = head.next
+#         n = len(vals)
         
-        for i, val in enumerate(vals):
-            if pair[n-i-1]:
-                pair[n-i-1] += val
-            else:
-                pair[i] += val
+#         for i, val in enumerate(vals):
+#             if pair[n-i-1]:
+#                 pair[n-i-1] += val
+#             else:
+#                 pair[i] += val
         
-        return max(pair.values())
+#         return max(pair.values())
