1c22bc37627f55769b9966ff2b241c0f3b40d519
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-07-08 00:04:55 -0700

    Time: 88 ms (48.84%), Space: 28.9 MB (16.90%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -13,22 +13,61 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        cur = root
+        # def _dfs(root,p,q,res,anc):
+            cur = root
+
+            if cur == p or cur == q: 
+                return cur
+
+            left, right = None, None
+
+            if root.left:
+                left = self.lowestCommonAncestor(root.left,p,q)
+            if root.right:
+                right = self.lowestCommonAncestor(root.right,p,q)
+
+            if left and right: 
+                return cur
+            elif left: 
+                return left 
+            else: 
+                return right
+            
+#             if len(res) < 2 and (root == p or root == q): 
+#                 res.append(root)
+#                 # if len(res) >= 2:
+#                 return res
+            
+#             if not root.left and not root.right: return []
+            
+#             right, left = None, None
+            
+#             if len(res) < 2 and root.left:
+#                 left = _dfs(root.left,p,q,res,anc)
+#                 # print(root.val, len(res), res[0].val, left[0].val)
+#                 if left and left[0] == res[0] and len(res) == 1:
+#                     print("hit")
+#                     res[0] = root
+#                 print(root.val, res[0].val, len(res),left)
+#                 if len(res) >= 2:
+#                     return res
+                
+#             if len(res) < 2 and root.right:
+#                 right =  _dfs(root.right,p,q,res,anc)
+#                 print(res,left)
+#                 if len(res) == 2 and left and :
+#                     res = [root,res[1]]
+#                 print("after root:", res)
+#                 if len(res) >= 2:
+#                     return res
+            
+#             # if right: print(right)
+                
+#             return res
+            
+#         res = _dfs(root,p,q,[],root)
+#         # print(res)
+#         return res[0]
         
-        if cur == p or cur == q: 
-            return cur
-        
-        left, right = None, None
-        
-        if root.left:
-            left = self.lowestCommonAncestor(root.left,p,q)
-        if root.right:
-            right = self.lowestCommonAncestor(root.right,p,q)
-        
-        if left and right: 
-            return cur
-        elif left: 
-            return left 
-        else: 
-            return right
+            
         
