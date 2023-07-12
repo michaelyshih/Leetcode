79be7b202a3fe07d8ba23defdac77c4bf77d5755
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-07-11 19:28:52 -0700

    Time: 49 ms (48.67%), Space: 16.3 MB (80.70%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -8,4 +8,8 @@ class Solution:
         for word, val in d.items(): 
             if val == 1:
                 missing.append(word)
+        # for i, word in enumerate(words):
+        #     if not d[word] == 1:
+        #         words.pop(i)
         return missing
+        # return words
