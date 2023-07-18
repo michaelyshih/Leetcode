5317df63be9961df959ae04f420eef67cf242eb8
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-07-18 11:50:27 -0700

    Time: 69 ms (51.36%), Space: 44.4 MB (31.16%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -18,11 +18,13 @@ var mergeTwoLists = function (l1, l2) {
 
     // If l1 is lower
     if (l1.val < l2.val) {
+        // console.log(l1)
         l1.next = mergeTwoLists(l1.next, l2);
         return l1;
     }
     // If l2 is lower
     else {
+        // console.log(l2)
         l2.next = mergeTwoLists(l1, l2.next);
         return l2;
     }
