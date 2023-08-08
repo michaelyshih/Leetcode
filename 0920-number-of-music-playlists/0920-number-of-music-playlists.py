0fa9bd017b2914cd7fe1ea7a34234e724f02a904
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-08-07 22:25:13 -0700

    Time: 65 ms (43.43%), Space: 16.7 MB (56.31%) - LeetHub

---
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -0,0 +1,22 @@
+# n songs 
+#  goal songs ( not necessary different - meaning can repeat )
+#  play all goals at least once 
+#  song can only play after k other songs have been played 
+#  return any combination modulo 10^9 + 7
+class Solution:
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        MOD = 10**9 + 7
+
+        # Initialize the DP table
+        dp = [[0 for _ in range(n + 1)] for _ in range(goal + 1)]
+        dp[0][0] = 1
+
+        for i in range(1, goal + 1):
+            for j in range(1, min(i, n) + 1):
+                # The i-th song is a new song
+                dp[i][j] = dp[i - 1][j - 1] * (n - j + 1) % MOD
+                # The i-th song is a song we have played before
+                if j > k:
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j] * (j - k)) % MOD
+
+        return dp[goal][n]
