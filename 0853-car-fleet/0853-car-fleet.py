55465ad6e853e89ea9352c438cc90ee2b6267a87
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-09-20 15:00:45 -0700

    Time: 970 ms (8.21%), Space: 38.1 MB (65.61%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,32 @@
+# positiove, speed => arr same length of n 
+# position[i] is position of i car and speed[i] us speed of i mph
+# cannot pass but stack 
+# distance is ignored => same position 
+# if same position becomes one fleet
+# if reach end together, still one fleet 
+# return number of car fleets at destination 
+
+# stack 
+# [3(1),0,0,5(3),0,4(1),0,0,2(4),0,1(2),0,end]
+# [0,1,0,0,0,0,0,4(1),0,0,0,0,1(2)]
+# cars position and speed iterate through cars starting from last position 
+# if next car pass prev car then merge 
+# if hits end then pop stack and result +1
+
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+#         organize of tuple of cars (position, speed)
+        carsV = [(p, s) for p, s in zip(position, speed)]
+        carsV.sort(reverse=True)
+#         some variable to store prev car 
+#         velocity target from the current position 
+#         divide that by speed of how many units will pass before hitting the end 
+        res = [] 
+        for p, s in carsV: 
+            carV = (target - p)/s
+            print(carV)
+            res.append(carV)
+            if len(res) >= 2 and res[-1] <= res[-2]:
+                res.pop()
+        return len(res)
