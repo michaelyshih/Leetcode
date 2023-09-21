7a3489543d7420ea671cdedd51af5d38607c5989
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-09-21 14:33:22 -0700

    Time: 55 ms (28.94%), Space: 41.1 MB (96.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -2,18 +2,20 @@ var climbStairs = function(n, memo = {}) {
 //     if (n == 1 || n == 0) return 1 // our base cases
 
 //     let first = 1;
-//     let second = 2;
+//     let last = 2;
 
 //     for (let i = 3; i <= n; i++) {
-//         let third = first + second;
-//         first = second;
-//         second = third;
+//         let next = first + last;
+//         first = last;
+//         last = next;
 //     }
-//     return second;
+//     return last;
 
     var fib = function(n){
         if (n == 1 || n == 0 ) return 1
+        
         if (memo[n]) return memo[n]
+        
         memo[n] = fib(n-1) + fib(n-2)
         // memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo)
         
@@ -21,4 +23,5 @@ var climbStairs = function(n, memo = {}) {
     }
     return fib(n)
 };
+
 
