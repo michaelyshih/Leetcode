5f29e757dcb5b60846ed914d7d32729b7eb51354
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-12-06 21:40:29 -0800

    Time: 1041 ms (30.34%), Space: 27.9 MB (61.65%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,22 +1,42 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        l,r = 0, 0
-        currSum = nums[r]
-        maxSum = 0
+        total = 0 
+        for i in range(k):
+            total += nums[i]
+        maxSum = total 
         
-        if len(nums) <= k:
-            return sum(nums) / k
-        while r < k -1: 
-            r += 1 
-            currSum += nums[r]
+        s,e = 0,k
+        
+        while e < len(nums):
+            total -= nums[s]
+            s+= 1
             
-        maxSum = currSum
-        
-        while r < len(nums) - 1:
-            r += 1 
-            currSum += nums[r]
-            currSum -= nums[l]
-            l += 1 
-            maxSum = max(maxSum, currSum)
+            total += nums[e]
+            e+=1
+            
+            maxSum = max(maxSum, total)
             
         return maxSum / k
+        
+        
+#         l,r = 0, 0
+#         currSum = nums[r]
+#         maxSum = 0
+        
+#         if len(nums) <= k:
+#             return sum(nums) / k
+#         while r < k -1: 
+#             r += 1 
+#             currSum += nums[r]
+            
+#         maxSum = currSum
+        
+#         while r < len(nums) - 1:
+#             r += 1 
+#             currSum += nums[r]
+#             currSum -= nums[l]
+#             l += 1 
+#             maxSum = max(maxSum, currSum)
+            
+#         return maxSum / k
+
