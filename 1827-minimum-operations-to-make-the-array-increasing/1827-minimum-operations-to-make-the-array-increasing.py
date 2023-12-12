e9486d4ba5782fea383a3f14fb95071848f327ba
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-12-11 20:54:33 -0800

    Time: 122 ms (31.49%), Space: 17.1 MB (13.75%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.py
@@ -1,10 +1,10 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        ans = 0
-        for i in range(1, len(nums)):
-            if nums[i] <= nums[i - 1]:
-                diff = abs(nums[i] - nums[i - 1]) + 1
-                nums[i] += diff
-                ans += diff
-
-        return ans
+        count = 0
+        for i in range(1,len(nums)):
+            if nums[i] <= nums[i-1]:
+                x = nums[i]
+                nums[i] += (nums[i-1] - nums[i]) + 1
+                count += nums[i] - x
+                
+        return count
