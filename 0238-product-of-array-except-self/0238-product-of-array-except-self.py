2075dc83d7ee2b7e818270096b0d0d1aecbcd874
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2023-12-13 11:05:29 -0800

    Time: 197 ms (48.93%), Space: 23.9 MB (75.91%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,10 +3,12 @@ class Solution:
         res = [1]* (len(nums))
         
         prefix = 1 
+#         first pass through to add all the prefix products into the res
         for i in range(len(nums)):
             res[i] = prefix 
             prefix *= nums[i]
         postfix = 1 
+#         second pass through on the pre-existing prefix to multiply it to the postfix product
         for i in range (len(nums) - 1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
