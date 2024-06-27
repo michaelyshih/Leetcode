6193cd37072931f4bc9df01bfcc2b4743c31be91
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2024-06-27 00:06:41 -0700

    Time: 34 ms (72.38%), Space: 16.4 MB (93.79%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         h = {chr(i+ ord("A") - 1):i for i in range(1,27)}
-        print(ord("A"))
         s = columnTitle
         n = len(s)
         
