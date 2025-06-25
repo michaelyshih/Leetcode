701ab05bae537e2116f875dc437b33162d44cca9
Michael Shih <109356854+michaelyshih@users.noreply.github.com>
2025-06-25 00:34:48 -0700

    Time: 0 ms (100%), Space: 18.8 MB (62.44%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i, num in enumerate(nums):
+            complement = target - num 
+            if complement in hash:
+                return [i, hash[complement]]
+            
+            hash[num] = i 
